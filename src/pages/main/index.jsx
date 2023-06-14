7dde9462ccf9e48b68179985d763dfd9f126a20d
LaraOrona <laraorona@outlook.com>
2023-06-14 20:15:43 -0300

    se añadieron cambios al proyecto

---
+++ src/pages/main/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from "../../components/nav-bar";
+
+const Main = () => {
+
+    return(
+        <div> 
+        <NavBar/>
+        </div>
+    )
+}
+
+export default Main
