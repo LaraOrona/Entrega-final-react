f774afb666948635adc8d54c58afb071221182bc
LaraOrona <laraorona@outlook.com>
2023-06-28 01:05:28 -0300

    se modifico el navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import Main from './pages/main';
+import NavBar from './components/nav-bar';
+////////////////////////////////////////
 
 
 
 function App() {
   return (
-    <div>
+    <NavBar />
 
-      <Main/>
 
-    </div>
-  );
+);
 }
 
 export default App;
--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import NavBar from "../../components/nav-bar";
+import { Box, Tab, Tabs } from '@mui/material';
+
 
 const Main = () => {
+    const [currentCategory, setCurrentCategory] = React.useState('cat1')
+
+    const handleChange = (_, value) => {
+        setCurrentCategory(value);
+    }
 
     return(
-        <div> 
-        <NavBar/>
-        </div>
+    <Box sx={{ width: '100%'}}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider '}}>
+            <Tabs value={currentCategory} aria-label="basic tabs example" onChange={handleChange}>
+                <Tab label= "Cat1" value={'cat1'} />
+                <Tab label= "Cat2" value={'cat2'}/>
+                <Tab label= "Cat3" value={'cat3'}/>
+            </Tabs>
+        </Box>
+    </Box>
     )
 }
 
